9bf9b33fb1461c30042c56aae376d3bea58ef61f
Soumyaranjanp68 <soumyaranjanp68@gmail.com>
2023-02-15 23:36:45 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
 import './App.css';
 
-function App(props) {
-  const [bio,setBio]=useState("");
-  return (
-    <div className="OuterContainer">
-      <textarea id="area" value={bio} 
-      onChange={(e)=>{
-        setBio(e.target.value)
-      }}></textarea>
-      <br/>
-      <button id="btn" onClick={()=>{
-        let newBio=bio
-        setBio(newBio.toUpperCase())
-      }}>Change To UpperCase
-      </button>
-      <p id="para">{bio}</p>
-    </div>
-    );
-}
+export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [gender, setGender] = useState('');
+  const [country, setCountry] = useState('');
+  const [marketing, setMarketing] = useState(false);
+  // console.log(marketing);
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-export default App;
+    if (firstName && lastName && gender && country && marketing) {
+      console.log(firstName);
+      console.log(lastName);
+      console.log(gender);
+      console.log(country);
+      console.log(marketing);
+    } else {
+      alert('Please enter all fields');
+    }
+  };
+  return (
+    <div>
+      <h1>User Registration Form</h1>
+      <form onSubmit={handleSubmit}>
+        <section>
+          <label>First Name</label>
+          <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </section>
+        <section>
+          <label>Last Name</label>
+          <input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </section>
+        <section>
+          <label>Gender</label>
+          <input
+            onChange={(e) => setGender(e.target.value)}
+            type="radio"
+            value="male"
+            checked={gender === 'male'}
+          />{' '}
+          Male
+          <input
+            onChange={(e) => setGender(e.target.value)}
+            type="radio"
+            value="female"
+            checked={gender === 'female'}
+          />{' '}
+          Female
+        </section>
+        <section>
+          <label>Country</label>
+          <select value={country} onChange={(e) => setCountry(e.target.value)}>
+            <option>Select an option...</option>
+            <option value="India">India</option>
+            <option value="USA">USA</option>
+            <option value="Australia">Australia</option>
+            <option value="UK">UK</option>
+          </select>
+        </section>
+        <section>
+          <label>Do you want to receive marketing emails?</label>
+          <input
+            type="checkbox"
+            value={marketing}
+            onChange={() => setMarketing(!marketing)}
+          />
+        </section>
+        <button type="submit">Submit</button>
+      </form>
+   </div>
+ );
+}
